8fd642417fd7a58018462374af600b7b2b99b6df
Your name <Your email>
2026-05-24 20:57:49 +0200

    updated file system FD

--- Teofil/run_plotting.py
+++ Teofil/run_plotting.py
@@ -74,13 +74,13 @@ def run_ZRT(directory: str, results_root: str, subdir: str = 'max_I_z', ylim=Non
 
 
 # ZXY, gaussian, no noise
-# run_ZXY(
-    # '/home/teofil/Desktop/Eldyn_sims/Simulations/FFT/zxy/gaussian/no_noise/',
-    # '/media/teofil/Data/Teofil/Ecole/_S04/ELDYN/Project/Our_project/Code/multiple_laser_filamentation/Results/FFT/zxy/gaussian/no_noise/',
-    # np.arange(0.0, 4.0, 0.079611),
-    # subdir='dynamic',
-    # ylim_I=None, vmax_zx=None, vmax_zy=None, vmax_xy=False,
-# )
+run_ZXY(
+    '/home/teofil/Desktop/Eldyn_sims/Simulations/FFT/zxy/gaussian/no_noise/',
+    '/media/teofil/Data/Teofil/Ecole/_S04/ELDYN/Project/Our_project/Code/multiple_laser_filamentation/Results/FFT/zxy/gaussian/no_noise/',
+    np.arange(0.0, 4.0, 0.079611),
+    subdir='dynamic',
+    ylim_I=None, vmax_zx=None, vmax_zy=None, vmax_xy=False,
+)
 
 # ZXY, gaussian, with noise
 # run_ZXY_Noise(
@@ -116,30 +116,22 @@ def run_ZRT(directory: str, results_root: str, subdir: str = 'max_I_z', ylim=Non
     # ylim=None,
 # )
 
-# gaussian, plasma
-run_ZRT(
-    '/home/teofil/Desktop/Eldyn_sims/Simulations/FD/zrt/no_plasma/',
-    '/media/teofil/Data/Teofil/Ecole/_S04/ELDYN/Project/Our_project/Code/multiple_laser_filamentation/Results/FD/zrt/no_plasma/',
-    subdir='dynamic',
-    ylim=None,
-)
+# ZRT, gaussian, no plasma
+# run_ZRT(
+    # '/home/teofil/Desktop/Eldyn_sims/Simulations/FD/zrt/no_plasma/',
+    # '/media/teofil/Data/Teofil/Ecole/_S04/ELDYN/Project/Our_project/Code/multiple_laser_filamentation/Results/FD/zrt/no_plasma/',
+    # subdir='dynamic',
+    # ylim=None,
+# )
 
+# ZRT, gaussian, plasma
+# run_ZRT(
+    # '/home/teofil/Desktop/Eldyn_sims/Simulations/FD/zrt/plasma/',
+    # '/media/teofil/Data/Teofil/Ecole/_S04/ELDYN/Project/Our_project/Code/multiple_laser_filamentation/Results/FD/zrt/plasma/',
+    # subdir='dynamic',
+    # ylim=None,
+# )
 
-########## Some tests
-# sim_test = BeamSimulationZXY_hermite('/home/teofil/Desktop/Eldyn_sims/test/Pin_014p0_Pcr_Hermite_4D_FFT_diagnostics.npz', '/home/teofil/Desktop/Eldyn_sims/test/', '/home/teofil/Desktop/Eldyn_sims/test/')
-# sim_test.on_axis_max_vs_z()
-# print(sim_test.snap_z)
-# sim_test.profile_zx()
-# sim_test.profile_zy()
-# sim_test.profile_xy(1.5)
-# for z in sim_test.snap_z:
-    # sim_test.profile_xy(z)
-# sim_test.profile_x(0.5)
-# for z in sim_test.snap_z:
-    # sim_test.profile_x(z)
-# sim_test.spectrum_xy(0)
-# print(sim_test.snap_z)
-# print(np.size(sim_test.snap_z))
 
 
 
@@ -192,30 +184,6 @@ def compare_on_axis_max_vs_z(sims: list, res_dir: str, name: str = 'compare_max_
     fig.savefig(os.path.join(res_dir, f'{name}.png'), dpi=150, bbox_inches="tight")
     plt.close(fig)
 
-
-# for pin_factor in [1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9]:
-    # compare_profile_x_gaussian(pin_factor, zs=[0.239, 0.955, 1.194, 1.433])
-# compare_profile_x_square(pin_factor=20.0, zs=[0.159, 0.318, 0.955])
-
-'''
-sim_clean = BeamSimulationZXY(
-    '/home/teofil/Desktop/Eldyn_sims/Simulations/FFT/zxy/gaussian/no_noise/many_diags/Pin_018p0_Pcr_gaussian_4D_FFT_diagnostics.npz',
-    '/home/teofil/Desktop/Eldyn_sims/Simulations/FFT/zxy/gaussian/no_noise/many_diags/',
-    '/media/teofil/Data/Teofil/Ecole/_S04/ELDYN/Project/Our_project/Code/multiple_laser_filamentation/Results/FFT/zxy/gaussian/a_manual/'
-)
-sim_noisy = BeamSimulationZXY_Noise(
-    '/home/teofil/Desktop/Eldyn_sims/Simulations/FFT/zxy/gaussian/noise/Pin_018p0_Pcr_xy_no_t_memory_optimized_noise.npz',
-    '/home/teofil/Desktop/Eldyn_sims/Simulations/FFT/zxy/gaussian/noise/',
-    '/media/teofil/Data/Teofil/Ecole/_S04/ELDYN/Project/Our_project/Code/multiple_laser_filamentation/Results/FFT/zxy/gaussian/a_manual/'
-)
-compare_on_axis_max_vs_z(
-    sims=[sim_clean, sim_noisy],
-    res_dir='/media/teofil/Data/Teofil/Ecole/_S04/ELDYN/Project/Our_project/Code/multiple_laser_filamentation/Results/FFT/zxy/gaussian/a_manual/',
-    name='compare_18p0Pcr'
-)
-# '''
-
-'''
 def compare_on_axis_max_vs_z_ZXT(sims: list, res_dir: str, name: str = 'compare_max_I_vs_z'):
     """
     Parameters
@@ -237,6 +205,29 @@ def compare_on_axis_max_vs_z_ZXT(sims: list, res_dir: str, name: str = 'compare_
     fig.savefig(os.path.join(res_dir, f'{name}.pdf'), dpi=150, bbox_inches="tight")
     fig.savefig(os.path.join(res_dir, f'{name}.png'), dpi=150, bbox_inches="tight")
     plt.close(fig)
+
+'''
+for pin_factor in [1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9]:
+    compare_profile_x_gaussian(pin_factor, zs=[0.239, 0.955, 1.194, 1.433])
+compare_profile_x_square(pin_factor=20.0, zs=[0.159, 0.318, 0.955])
+'''
+
+'''
+sim_clean = BeamSimulationZXY(
+    '/home/teofil/Desktop/Eldyn_sims/Simulations/FFT/zxy/gaussian/no_noise/many_diags/Pin_018p0_Pcr_gaussian_4D_FFT_diagnostics.npz',
+    '/home/teofil/Desktop/Eldyn_sims/Simulations/FFT/zxy/gaussian/no_noise/many_diags/',
+    '/media/teofil/Data/Teofil/Ecole/_S04/ELDYN/Project/Our_project/Code/multiple_laser_filamentation/Results/FFT/zxy/gaussian/a_manual/'
+)
+sim_noisy = BeamSimulationZXY_Noise(
+    '/home/teofil/Desktop/Eldyn_sims/Simulations/FFT/zxy/gaussian/noise/Pin_018p0_Pcr_xy_no_t_memory_optimized_noise.npz',
+    '/home/teofil/Desktop/Eldyn_sims/Simulations/FFT/zxy/gaussian/noise/',
+    '/media/teofil/Data/Teofil/Ecole/_S04/ELDYN/Project/Our_project/Code/multiple_laser_filamentation/Results/FFT/zxy/gaussian/a_manual/'
+)
+compare_on_axis_max_vs_z(
+    sims=[sim_clean, sim_noisy],
+    res_dir='/media/teofil/Data/Teofil/Ecole/_S04/ELDYN/Project/Our_project/Code/multiple_laser_filamentation/Results/FFT/zxy/gaussian/a_manual/',
+    name='compare_18p0Pcr'
+)
 # '''
 
 '''
@@ -262,66 +253,3 @@ compare_on_axis_max_vs_z_ZXT(
     name='compare_square'
 )
 # '''
-
-# plot all files in all the dirs in a list of paths
-'''
-def plot_from_dirs(paths: list[str]):
-    for path in paths:
-        for file in os.listdir(path):
-            if file.endswith('.npz'):
-                sim = BeamSimulationXYZ(os.path.join(path, file))
-                sim.on_axis_max_vs_z()
-                sim.profile_xz()
-                for z in np.arange(0.0, 4.0, 1.0): # default = 0.079611
-                    sim.profile_x(z)
-                    sim.profile_xy(z)
-                    # sim.spectrum_xy(z)
-
-plot_from_dirs([
-    '/media/teofil/Data/Teofil/Ecole/_S04/ELDYN/Project/Our_project/Code/multiple_laser_filamentation/Teofil/test_runs/'
-])
-#'''
-
-
-'''
-########## Old test data
-def data1():  # easy test
-    xmin, xmax = -0.1, 0.1
-    Nx = 50
-    ymin, ymax = -0.1, 0.1
-    Ny = 50
-    zmin, zmax = 0.0, 10.0
-    Nz = 100
-
-    x = np.linspace(xmin, xmax, Nx)
-    y = np.linspace(ymin, ymax, Ny)
-    z = np.linspace(zmin, zmax, Nz)
-    X, Y = np.meshgrid(x, y)
-    Emesh = np.zeros((Nz, Ny, Nx))
-    for i in range(Nz):
-        Emesh[i] = supergaussian(np.sqrt(X ** 2 + Y ** 2), Pin=gaussian(r=z[i] - zmax / 3), width=0.05, p=5)
-    return (xmin, xmax, Nx, ymin, ymax, Ny, zmin, zmax, Nz, Emesh)
-
-def data2(): # heavier test
-    xmin, xmax = -0.1, 0.1
-    Nx = 500
-    ymin, ymax = -0.1, 0.1
-    Ny = 500
-    zmin, zmax = 0.0, 10.0
-    Nz = 1000
-
-    x = np.linspace(xmin, xmax, Nx)
-    y = np.linspace(ymin, ymax, Ny)
-    z = np.linspace(zmin, zmax, Nz)
-    X, Y = np.meshgrid(x, y)
-    Imesh = np.zeros((Nz, Ny, Nx))
-
-    for i in range(Nz):
-        Imesh[i] = gaussian(np.sqrt(X ** 2 + Y ** 2), Pin=gaussian(r=z[i] - zmax / 3), width=0.05)
-    return (xmin, xmax, Nx, ymin, ymax, Ny, zmin, zmax, Nz, Imesh)
-
-# args = data1()
-# on_axis_max_I_vs_z(args)
-# profile_yz(args)
-# field_in_time(args, tmin=-200e-15, tmax=200e-15, Nt=1000, z=3.4)
-'''
